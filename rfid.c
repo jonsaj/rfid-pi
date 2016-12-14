1752a87003c689aaae43004b4f5ee5bcbab667ab
jon <jon@gmail.com>
2016-12-14 15:34:41 -0500

    SPI test

--- rfid.c
+++ rfid.c
@@ -13,5 +13,21 @@ void main(){
 		printf("bcm2835_spi_begin failed. Root maybe?\n");
 		return 1;
 	}
-	bcm2835_spi_setBitOrder(
+	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);
+	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0); 
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536);
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);
+
+    // Send a byte to the slave and simultaneously read a byte back from the slave
+    // If you tie MISO to MOSI, you should read back what was sent
+    uint8_t send_data = 0x23;
+    uint8_t read_data = bcm2835_spi_transfer(send_data);
+    printf("Sent to SPI: 0x%02X. Read back from SPI: 0x%02X.\n", send_data, read_data);
+    if (send_data != read_data)
+      printf("Do you have the loopback from MOSI to MISO connected?\n");
+    bcm2835_spi_end();
+    bcm2835_close();
+    return 0;
+
 }
