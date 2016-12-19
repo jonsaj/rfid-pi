ea82d634f6bbd0e9eb7fd93954579b7e6b8dc8b0
jon <jon@gmail.com>
2016-12-19 01:37:31 -0500

    partially implemetned self test

--- rfid.c
+++ rfid.c
@@ -7,12 +7,13 @@
 #define SPI_CHANNEL	0
 #define SPI_SPEED	1000000
 
-
 //register definitions
+#define REG_COMMAND	0x01
+#define REG_COMINTRQEN	0x02
+
 #define REG_BITFRAMING	0x0D
 #define REG_COMIRQ	0x04
-#define REG_COMIRQEN	0x02
-#define REG_COMMAND	0x01
+
 #define REG_ERROR	0x06
 #define REG_FIFODATA	0x09
 #define REG_FIFOLEVEL	0x0A
@@ -26,6 +27,7 @@
 #define REG_TXCONTROL	0x14
 #define REG_TXMODE	0x12
 #define REG_VERSION	0x37
+#define REG_AUTOTEST	0x36
 
 //Commands
 //pcd
@@ -37,10 +39,7 @@
 #define COMM_NOCMDCHG	0x07
 #define COMM_RECEIVE	0x08
 #define COMM_TRANSVE	0x0C
-
-
 #define COMM_MIFAUTH	0x0E
-
 #define COMM_SOFTRESET	0x15
 
 //picc
@@ -54,6 +53,8 @@
 #define COMM_SELECT_1	0x93
 #define COMM_SELECT_2	0x70
 #define	COMM_WRITE	0xA0
+
+#define TEST_SELFTEST	0x09
 
 //picc responses
 #define RESP_ANSRQ	0x04
@@ -74,6 +75,10 @@ void setAddr(char *data, const char addr){
 
 void clearLSB(char *data){
 	data[0] = data[0] & 0xfe;
+}
+
+void setComm(char *data, const char comm){
+	data[1] = (data[1] & ~(0xF)) & (0xF & comm);
 }
 
 void printBits(const char* message, char data){
@@ -122,9 +127,48 @@ void getVersion(){
 	printf("Version: %i.0\n", data & 0xf);	
 }
 
+void softReset(){
+	unsigned char data;
+	clearLSB(&data);
+	setWrite(&data);
+	setAddr(&data,REG_COMMAND);
+	setComm(&data,COMM_SOFTRESET);
+	wiringPiSPIDataRW(SPI_CHANNEL, &data, 2);
+	//usleep(500000); //unsure if sleep needed after softreset
+	//considering that commandReg register has a PowerDown
+	// soft power down field, I think no delay is needed
+}
+
+void clearInternalBuffer(){
+//clear FIFO buffer
+	//setAddr(&data,
+	setComm(&data, COMM_MEM);
+	wiringPiSPIDataRW(SPI_CHANNEL, &data, 1);
+}
+
+void rfidSelfTest(){
+	softReset();
+	clearBuffer();
+
+	unsigned short testBytesCount = 64;
+	unsigned char data[testBytesCount];
+	data[0] = 0x0;
+	setAddr(&data,REG_AUTOTEST);
+	setWrite(&data);
+	clearLSB(&data);
+	data[1] = data[1] & TEST_SELFTEST;
+	int i;
+	for(i=0;i<testBytesCount;i++){
+		wiringPiSPIDataRW(SPI_CHANNEL, &data, testBytesCount);	
+	}
+
+}
+
 int main(){
 	spiInit();
-	getVersion();	
+	getVersion();
+
+
 	return 0;
 }
 
